30e96eb64c54cc2a491e1da7fb70a39a3a8c39b6
emilymorgenbesser <morgenbesseremily@gmail.com>
2022-05-02 13:09:25 -0400

    1

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-const express = require('express')();
+const app = require('express')();
+const PORT = 8080;
+
+app.get('/tshirt', (req, res) => {
+    res.status(200).send({
+        tshirt: '👚',
+        size: 'large'
+    })
+});
+
+app.post('/tshirt/:id', (req, res) => {
+
+    const { id } = req.params;
+    const { logo } = req.body;
+
+    if(!logo) {
+        res.status(418).send({ message: "No logo available!" })
+    }
+
+    res.send({
+        tshirt: `👚 with your ${logo} and ID of ${id}`,
+    })
+});
+
+app.listen(
+    PORT,
+    () => console.log(`it's alive on http://localhost:${PORT}`)
+)
